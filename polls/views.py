e9334bd8894a37da59dbee4aad6473caba12cebf
jyo8141 <xyz@email.com>
2023-01-24 00:02:36 +0400

    Part5 and Part6

--- polls/views.py
+++ polls/views.py
@@ -46,18 +46,18 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
-# def results(request, question_id):
-#     response = "You're looking at the results of question %s."
-#     return render(request, response % question_id)
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return render(request, response % question_id)
 
-# def vote(request, question_id):
-#     return render(request, "You're voting on question %s." % question_id)
+def vote(request, question_id):
+    return render(request, "You're voting on question %s." % question_id)
